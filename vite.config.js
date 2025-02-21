c38f685872da691843c3fbdb7e811ef2aabdd379
Ritul Dani <rituldani2003@gmail.com>
2025-02-21 21:32:38 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // plugins: [react()],
-  base: '/Chat-API/', 
+  plugins: [react()],
+  base: '/Chat-API', 
 });
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: './', // Important for relative paths
-//   build: {
-//     outDir: "dist",
-//   },
-// });
-
